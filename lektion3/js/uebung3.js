94e5982dc3b61f76199540448ea05c3be77e8e9a
Alexey <allxyog@gmail.com>
2022-04-28 12:26:57 +0200

    Erledigt

--- lektion3/js/uebung3.js
+++ lektion3/js/uebung3.js
@@ -6,50 +6,64 @@
  * 1) Erstelle die Map.
  */
 function aufgabe1() {
-  console.debug("Aufgabe 1 ==========================");
+    console.debug("Aufgabe 1 ==========================");
 
-  //ToDo: füge ab hier Deinen Code ein
-
-  console.debug(pizza)
+    //ToDo: füge ab hier Deinen Code ein
+    let pizza = {
+        art: "Tonno",
+        belag: "Käse und Zwiebeln",
+        skala: 10,
+        gerneIsst: true,
+    }
+    console.debug(pizza)
 }
 
 /**
  * 2) Rufe keys, value und entries auf.
  */
 function aufgabe2() {
-  console.debug("Aufgabe 2 ==========================");
+    console.debug("Aufgabe 2 ==========================");
 
-  let ghibli = {
-    name: "Studio Ghibli",
-    gruendung: 1985,
-    medium: "Anime"
-  }
+    let ghibli = {
+        name: "Studio Ghibli",
+        gruendung: 1985,
+        medium: "Anime",
+    }
 
-  //ToDo: füge ab hier in den debugs Deinen Code ein
-  console.debug()
-  console.debug()
-  console.debug()
+    //ToDo: füge ab hier in den debugs Deinen Code ein
+    console.debug(Object.keys(ghibli))
+    console.debug(Object.values(ghibli))
+    console.debug(Object.entries(ghibli))
 }
 
 /**
  * 3) Erstelle eine weitere Map.
  */
 function aufgabe3() {
-  console.debug("Aufgabe 3 ==========================");
+    console.debug("Aufgabe 3 ==========================");
 
-  //ToDo: füge ab hier Deinen Code ein
+    //ToDo: füge ab hier Deinen Code ein
+    let kuenstler = {
+        vorname: "Alfredolini",
+        nachname: "Nudel",
+        status: "offline",
+        geboren: 1970,
+        hobby: "pumper",
+        taetigkeit: "Fitnessmodel"
+    }
 
-  for (let key of Object.keys(kuenstler)) {
-    console.debug(key + ": " + kuenstler[key])
-  }
+    for (let key of Object.keys(kuenstler)) {
+        console.debug(key + ": " + kuenstler[key])
+    }
 }
 
 /**
  * Hauptfunktion, in der die Aufgaben aufgerufen werden
  */
 function main() {
-  aufgabe1()
-  aufgabe2()
-  aufgabe3()
+    aufgabe1()
+    aufgabe2()
+    aufgabe3()
 }
+
 main()
